a9def987f545f2336b2c53de04503a9aba0024ee
TaeseoungKim <aka404365@gmail.com>
2021-08-23 00:31:41 +0900

    [feat] category 구현

--- src/components/investment/index.js
+++ src/components/investment/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Category from './components/Category.js';
 const index = () => {
     return (
-        <div>
+        <>
             <h1>Investment 페이지 입니다</h1>
-        </div>
+            <Category></Category>
+        </>
     );
 };
 
