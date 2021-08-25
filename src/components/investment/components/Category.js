b2eb64d3711f569375cc801c76dedce42c9ae038
TaeseoungKim <aka404365@gmail.com>
2021-08-26 03:38:25 +0900

    [feat] 투자 탭 카테고리 클릭시 색깔 변환 기능 추가

--- src/components/investment/components/Category.js
+++ src/components/investment/components/Category.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Category = () => {
 
+    const [selected, setSelected] = useState("stock")
+
+
+    const onClick = (e) => {
+
+        setSelected(e.target.id);
+        console.log("클릭발생", e);
+        console.log(e.target.id);
+    }
 
 
     return (
+
+
         <div className="category-container">
-            <ul id="category-ul">
-                <li id="category-li">주식</li>
-                <li id="category-li">비트코인</li>
-                <li id="category-li">예적금</li>
-                <li id="category-li">부동산</li>
-                <li id="category-li">금</li>
-            </ul>
+            <div id="category-ul">
+                <div className={selected == "stock" ? "selected-li" : "unSelected-li"} id="stock" onClick={onClick}> 주식</div>
+                <div className={selected == "coin" ? "selected-li" : "unSelected-li"} id="coin" onClick={onClick}>암호화폐</div>
+                <div className={selected == "deposit" ? "selected-li" : "unSelected-li"} id="deposit" onClick={onClick}>예·적금</div>
+                <div className={selected == "realestate" ? "selected-li" : "unSelected-li"} id="realestate" onClick={onClick}>부동산</div>
+                <div className={selected == "gold" ? "selected-li" : "unSelected-li"} id="gold" onClick={onClick}>금</div>
+            </div>
         </div >
     );
 };
--- src/components/investment/components/ItemList/ItemStorage.js
+++ src/components/investment/components/ItemList/ItemStorage.js
@@ -1,35 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 //https://search.pstatic.net/sunny/?src=https%3A%2F%…%2F_images%2Ffavicon.ico&type=f30_30_png_expire24 삼성이미지
-
+// 라이프 사이클,
+// useeffect 렌더링 전 , 후 표시?
+// 렌더링 되기 전 데이터를 받아와야 함
 const ItemStorage = () => {
 
-    const [Items, setItem] = React.useState([
+    //이름, 코드?(필요한가), 값, rate만 가져올 수 있도록한다.
+    const [Items, setItem] = useState([
         {
-            id: '1',
-            name: '삼성전자',
-            imageUrl: 'https://cdn.pixabay.com/photo/2016/12/22/04/28/facebook-1924510_960_720.png',
-            price: 79200,
-            changedPrice: 4580,
-            changedpercent: 5.1,
-        },
-        {
-            id: '2',
-            name: '엘지',
-            imageUrl: 'https://cdn.pixabay.com/photo/2016/12/22/04/28/facebook-1924510_960_720.png',
-            price: 79200,
-            changedPrice: 4580,
-            changedpercent: 5.1,
+            code: "",
+            endValue: 0,
+            highValue: 0,
+            lowValue: 0,
+            name: "0",
+            rate: 0,
+            startValue: 0,
+            tradeAmount: 0,
+            value: 0,
         },
     ]);
+    /*
+
+    */
+    useEffect(() => {
+        // updateData() => setInterval 10초마다 장이열리는 시간이면 ㅋㅋ
+        axios.get('/stock/real-data').then(res => {
+            console.log(res.data);
+            let newArr = res.data.map((item, i) => {
+                return { item };
+            });
+            console.log("res.data 입니다");
+            console.log(res.data);
+            setItem(res.data);
+        });
+    }, []);
+
+    const fluctuationCal = (value, rate) => {
+        let prevValue = value / (1 + (rate / 100))
+        return value - prevValue;
+    }
+    let sibal = {
+        display: "#FF0000",
+    }
 
     return (
         <>
             {Items.map((item, index) => {
+                const positive = item.rate > 0 ? true : false
                 return (
                     < div className="item" id="item-container" key={item.id} >
 
                         <div className="item" id="item-img">
-                            <img className="item" src={item.imageUrl} alt={item.name} />
+                            {/* <img className="item" src={item.imageUrl} alt={item.name} /> */}
                         </div>
 
                         <div className="item" id="item-name">
@@ -37,12 +60,12 @@ const ItemStorage = () => {
                         </div>
 
                         <div className="item" id="item-price">
-                            {item.price.toLocaleString('ko-KR')} TYL
+                            {parseInt(item.value).toLocaleString('ko-KR')} TYL
                         </div>
 
                         <div className="item" id="item-changed">
-                            <div id="item-changedprice">+{item.changedPrice.toLocaleString('ko-KR')}</div>
-                            <div id="item-changedpercent">({item.changedpercent}%)</div>
+                            <div id="item-changedprice" style={positive > 0 ? { color: '#FF0000' } : { color: '#001AFF' }}>{positive > 0 ? "+" : ""}{parseInt(fluctuationCal(item.value, item.rate)).toLocaleString('ko-KR')}</div>
+                            <div id="item-changedpercent" style={positive > 0 ? { color: '#FF0000' } : { color: '#001AFF' }}>({item.rate}%)</div>
                         </div>
 
 
