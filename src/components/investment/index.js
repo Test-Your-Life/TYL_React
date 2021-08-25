e4cd37adabef368678471f1cf001d5fbdca47cd3
TaeseoungKim <aka404365@gmail.com>
2021-08-25 21:28:05 +0900

    [feat] 풀 리퀘 전 PUSH

--- src/components/investment/index.js
+++ src/components/investment/index.js
@@ -14,6 +14,7 @@ const index = () => {
             <Chart></Chart>
             <Prediction></Prediction>
             <Trade></Trade>
+            <div id="empty-space"></div>
         </>
     );
 };
