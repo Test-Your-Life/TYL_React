cdc2c16e9ac7332bf4d6642ed1fa2ee500e1affd
임대호 <eogh0959@gmail.com>
2021-09-02 01:50:09 +0900

    [feat] 랭킹 select 구현

--- src/components/ranking/component/container/AllTab.js
+++ src/components/ranking/component/container/AllTab.js
@@ -12,7 +12,7 @@ const AllTab = () => {
     });
   }, []);
 
-  const peopleList = [
+  const assetList = [
     { rank: '1위', nk_name: 'KIM', asset: '400,000,000틸' },
     { rank: '2위', nk_name: 'LEE', asset: '300,000,000틸' },
     { rank: '3위', nk_name: 'LIM', asset: '200,000,000틸' },
@@ -21,9 +21,18 @@ const AllTab = () => {
     { rank: '6위', nk_name: 'PARK', asset: '10,000,000틸' },
   ];
 
+  const yieldList = [
+    { rank: '1위', nk_name: '안녕', asset: '48.3%' },
+    { rank: '2위', nk_name: '주식부자', asset: '39.2%' },
+    { rank: '3위', nk_name: '호호', asset: '36.1%' },
+    { rank: '4위', nk_name: '임대호', asset: '29.1%' },
+    { rank: '5위', nk_name: '쀼시닝', asset: '25.8%' },
+    { rank: '6위', nk_name: '시계나이', asset: '12.4%' },
+  ];
+
   return (
     <>
-      <AllRank info={peopleList} />
+      <AllRank ainfo={assetList} yinfo={yieldList} />
     </>
   );
 };
--- src/components/ranking/component/presentational/AllRank.js
+++ src/components/ranking/component/presentational/AllRank.js
@@ -2,16 +2,47 @@ import React, { useState } from 'react';
 import search_img from '../../../../styles/images/search_icon.png';
 import RankList from './RankList';
 
-const AllRank = ({ info }) => {
+const AllRank = ({ ainfo, yinfo, props }) => {
+  const [selected, setSelected] = useState('total-asset');
   const [inputValue, setInputValue] = useState('');
+
+  const getItem = item => {
+    props.getItem(item);
+  };
 
   const onSubmit = e => {
     e.preventDefault();
   };
 
+  const onChangeSelectOptionHandler = e => {
+    setSelected(e.target.value);
+  };
+
   const onChangeInput = e => {
     setInputValue(e.target.value);
   };
+
+  // 선택된 아이템에 따라 값을 비교하고 데이타를 리턴
+  function Inquiries() {
+    if (selected == 'total-asset') {
+      return <RankList info={ainfo} />;
+    } else if (selected == 'yield') {
+      return <RankList info={yinfo} />;
+    }
+  }
+
+  const SelectTag = (
+    <>
+      <select className="select-container" onChange={onChangeSelectOptionHandler}>
+        <option className="select-option" selected value="total-asset">
+          총자산
+        </option>
+        <option className="select-option" value="yield">
+          수익률
+        </option>
+      </select>
+    </>
+  );
 
   const SearchForm = (
     <>
@@ -22,7 +53,6 @@ const AllRank = ({ info }) => {
           type="text"
           value={inputValue}
           placeholder="유저를 찾아보세요"
-          autoFocus
           onChange={onChangeInput}
         ></input>
       </form>
@@ -32,8 +62,9 @@ const AllRank = ({ info }) => {
   return (
     <>
       <ul className="ranking-container">
-        {SearchForm}
-        <RankList info={info} />
+        <div>{SelectTag}</div>
+        <div>{SearchForm}</div>
+        <div><Inquiries/></div>
       </ul>
     </>
   );
