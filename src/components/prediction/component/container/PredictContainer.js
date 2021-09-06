db896c8499733426536bcdd2ee85df2856605c2b
임대호 <eogh0959@gmail.com>
2021-09-06 23:56:55 +0900

    [fix] 랭킹 탭 warning 해결

--- src/components/prediction/component/container/PredictContainer.js
+++ src/components/prediction/component/container/PredictContainer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Predict from '../presentation/Predict';
 import { useMediaQuery } from 'react-responsive';
+import Predict from '../presentation/Predict';
 
 const PredictContainer = () => {
   const isPc = useMediaQuery({
